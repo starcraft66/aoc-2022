f3eb1248e257e9b0f01b5b2f64cd5ee40c53fb8a
Tristan Gosselin-Hane <starcraft66@gmail.com>
2022-12-02 01:22:13 -0500

    use atoms dummy

--- day2/day2p2.exs
+++ day2/day2p2.exs
@@ -15,23 +15,23 @@ defmodule Day2 do
 
   def points(move) do
     case move do
-      "rock" -> 1
-      "paper" -> 2
-      "scissors" -> 3
+      :rock -> 1
+      :paper -> 2
+      :scissors -> 3
     end
   end
 
   def outcome(opponent, result) do
     case {opponent, result} do
-      {"A", "X"} -> points("scissors") + 0
-      {"A", "Y"} -> points("rock") + 3
-      {"A", "Z"} -> points("paper") + 6
-      {"B", "X"} -> points("rock") + 0
-      {"B", "Y"} -> points("paper") + 3
-      {"B", "Z"} -> points("scissors") + 6
-      {"C", "X"} -> points("paper") + 0
-      {"C", "Y"} -> points("scissors") + 3
-      {"C", "Z"} -> points("rock") + 6
+      {"A", "X"} -> points(:scissors) + 0
+      {"A", "Y"} -> points(:rock) + 3
+      {"A", "Z"} -> points(:paper) + 6
+      {"B", "X"} -> points(:rock) + 0
+      {"B", "Y"} -> points(:paper) + 3
+      {"B", "Z"} -> points(:scissors) + 6
+      {"C", "X"} -> points(:paper) + 0
+      {"C", "Y"} -> points(:scissors) + 3
+      {"C", "Z"} -> points(:rock) + 6
     end
   end
 end
